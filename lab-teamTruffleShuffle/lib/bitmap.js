3c636ff40a09bb219144797a9c7026f0b630a6dd
Tim Turner <timmyturner@me.com>
2017-08-25 14:59:18 -0700

    adds in callback functions and stuff

--- lab-teamTruffleShuffle/lib/bitmap.js
+++ lab-teamTruffleShuffle/lib/bitmap.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const fs = require('fs');
-const transform = require('./transform');
-const readWrite = require('./read-write');
+//const fs = require('fs');
+//const transform = require('./transform');
+//const readWrite = require('./read-write');
 
-const bitmap = module.exports = {};
-
-function Bitmap(buffer) {
+module.exports = function(buffer) {
   this.allData = buffer;
   this.type = buffer.toString('utf-8', 0, 2);
   this.fileSize = buffer.readInt32LE(2);
@@ -14,26 +12,4 @@ function Bitmap(buffer) {
   this.width = buffer.readInt32LE(18);
   this.height = buffer.readInt32LE(22);
   this.pixelArray = buffer.slice(54, this.offset);
-}
-
-function newBmp(dataObj) {
-  let bmp = new Bitmap(dataObj);
-  return bmp;
-}
-
-bitmap.newBitmapInverted = (fileName, callback) => {
-  readWrite.readFilesAsync(fileName).then(newBmp(data))
-    .then(transform.invert(bmp.pixelArray))
-    .then(readWrite.writeFilesAsync(fileName + 'inverted', bmp.pixelArray))
-    .catch(function(err) {
-      console.error(err);
-    });
-}
-
-bitmap.newBitmapGrayScale = function() {
-  let bmp = new Bitmap(readWrite.readFilesAsync(fileName));
-}
-
-bitmap.newBitmapBlackOut = function() {
-  let bmp = new Bitmap(readWrite.readFilesAsync(fileName));
-}
+};
--- lab-teamTruffleShuffle/lib/read-write.js
+++ lab-teamTruffleShuffle/lib/read-write.js
@@ -1,34 +1,55 @@
 'use strict';
 
-
-//const Bitmap = require('./bitmap');
 const fs = require('fs');
+const Bitmap = require('./bitmap');
 
 const readWrite = module.exports = {};
 
-readWrite.readFilesAsync = (fileName) => {
-  return new Promise((resolve,reject) => {
-    fs.readFile(fileName, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
+readWrite.readFiles = (fileName, done) => {
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      return done(err);
+    } else {
+      let bmp = new Bitmap(data);
+      return done(bmp);
+    }
   });
 };
 
-readWrite.writeFilesAsync = (fileName, fileData) => {
-  return new Promise((resolve,reject) => {
-    fs.writeFile(fileName, fileData, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(console.log('New file created'));
-      }
-    });
+readWrite.writeFiles = (fileName, fileData, done) => {
+  fs.writeFile(fileName, fileData, (err) => {
+    if (err) {
+      return done(err);
+    } else {
+      return console.log('New bitmap file created.');
+    }
   });
 };
+
+
+// readWrite.readFilesAsync = (fileName) => {
+//   return new Promise((resolve,reject) => {
+//     fs.readFile(fileName, (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+//
+// readWrite.writeFilesAsync = (fileName, fileData) => {
+//   return new Promise((resolve,reject) => {
+//     fs.writeFile(fileName, fileData, (err) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(console.log('New file created'));
+//       }
+//     });
+//   });
+// };
 
 
 //readWrite.readFiles();
